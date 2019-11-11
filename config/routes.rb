174d352fe5ed73eb864184e5b473fc62abec6702
clarkj99 <clark@stontech.net>
2019-11-11 16:44:40 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get "users/new"
+  get "users/show"
+
+  get "sessions/new"
+
+  post "sessions/create"
+  post "users/create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
